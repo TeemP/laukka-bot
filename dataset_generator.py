3ccad18f184fd0d6f0c459b9a02f3163c14af981
Teemu Pekkanen <teemu.pekkanen@hotmail.com>
2020-02-27 15:12:08 +0200

    The first lines of dataset_generator

---
+++ dataset_generator.py
@@ -0,0 +1,24 @@
+from os import path
+from pydub import AudioSegment
+
+# Read data
+def read_files(source_path, ending):
+    for file_name in os.listdir(source_path):
+        if file_name[-len(ending):] ==ending                                 
+        sound = AudioSegment.from_mp3(os.join(source_path,file_name))
+
+def write_files(audio_data_list, target_path):
+    for i,audio_data in enumerate(audio_data_list):
+        sound.export(os.join(target_path,i), format="mp3")
+
+def generate_dataset(source_audio_data_list):
+    source_audio_data_list_size = len(source_audio_data_list)
+    for i,source_audio_data in enumerate(source_audio_data_list):
+        for j in range(i,source_audio_data_list_size):
+            #todo mix audio
+
+def main():
+    # Paths
+    source_path = './data/source'
+    target_path = './data/target'                                                                       
+    ending = ".mp3"
