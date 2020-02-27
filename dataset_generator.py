7b71ca5f6a38ff795c2d7b133824b50194220547
Teemu Pekkanen <teemu.pekkanen@hotmail.com>
2020-02-27 15:19:33 +0200

    dataset generator program main lines

--- dataset_generator.py
+++ dataset_generator.py
@@ -3,9 +3,11 @@ from pydub import AudioSegment
 
 # Read data
 def read_files(source_path, ending):
+    data_list = []
     for file_name in os.listdir(source_path):
         if file_name[-len(ending):] ==ending                                 
-        sound = AudioSegment.from_mp3(os.join(source_path,file_name))
+            sound = AudioSegment.from_mp3(os.join(source_path,file_name))
+            data_list.append(sound)
 
 def write_files(audio_data_list, target_path):
     for i,audio_data in enumerate(audio_data_list):
@@ -22,3 +24,5 @@ def main():
     source_path = './data/source'
     target_path = './data/target'                                                                       
     ending = ".mp3"
+    write_files(generate_dataset(read_files(source_path, ending)))
+    print('Dataset generated to directory:',target_path)
